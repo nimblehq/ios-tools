2b55ace5039f2ca4dfe8fd9bf100312b1529a2ae
llleyelll <chida.fua@gmail.com>
2021-08-16 16:37:30 +0700

    [#1] Update folder naming

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@
 mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates
 mkdir -p ~/Library/Developer/Xcode/Templates/Project\ Templates
 mkdir -p ~/Library/Developer/Xcode/Templates/Project\ Templates/Nimble\ Templates
-cp -R resources/xcode-templates/* ~/Library/Developer/Xcode/Templates/Project\ Templates/Nimble\ Templates
+cp -R xcode-templates/xcode-templates/* ~/Library/Developer/Xcode/Templates/Project\ Templates/Nimble\ Templates
